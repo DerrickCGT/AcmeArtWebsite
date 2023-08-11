51b3775951964042fe2d14718874e906c94a009f
Francis Sullivan <francis.a.sullivan@proton.me>
2023-08-11 10:42:24 +0800

    Home page updated.

--- index.php
+++ index.php
@@ -13,9 +13,9 @@
         <?php
         include_once('navbar.php');
         ?>
-        <div class="container-fluid" style="height: 50vh;">
+        <div class="container-fluid"">
 
-            <div class="d-flex justify-content-center align-items-center" style="height: 45vh;">
+            <div class="d-flex flex-column justify-content-center align-items-center"">
                 <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
                     <div class="carousel-inner">
                         <div class="carousel-item active">
@@ -37,18 +37,16 @@
                         <span class="visually-hidden">Next</span>
                     </button>
                 </div>
-            </div>
-            <div class="d-flex justify-content-center align-items-center">
+            
 
                 <h2>AT2 Sprint 1 </h2>
-            </div>
-            <div class="d-flex justify-content-center align-items-center" style="height: 23vh;">
+        
 
                 <p class="lead">
-                    Team name: <img src="images\logo.png" class="img-thumbnail" alt="logo" width="80" height="80"> King Rabbit<br>
-                    Team Leader: Derrick Choong<br>
-                    Team Member #1: Francis Sullivan<br>
-                    Team Member #2: Dongyun Huang<br>
+                        Team name: <img src="images\logo.png" class="img-thumbnail" alt="logo" width="80" height="80"> King Rabbit<br>
+                        Team Leader: Derrick Choong<br>
+                        Team Member #1: Francis Sullivan<br>
+                        Team Member #2: Dongyun Huang<br>
                 </p>
             </div>
 
